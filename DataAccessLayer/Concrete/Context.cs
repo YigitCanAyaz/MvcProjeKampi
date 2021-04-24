c9ba6ccfb52d0e6c8716e618f3ce2968ddd102b6
Yigit Can Ayaz <yigitcanayaz2000@gmail.com>
2021-04-25 01:00:29 +0300

    birinci gorev

---
+++ DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,24 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    public class Context : DbContext
+    {
+        // SQL Tablo Kısmını Ayarlıyoruz
+
+        // About => Class'ın ismi
+        // Abouts => SQL'deki tablonun ismi
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Content> Contents { get; set; }
+        public DbSet<Heading> Headings { get; set; }
+        public DbSet<Writer> Writers { get; set; }
+    }
+}
---
+++ EntityLayer/Concrete/Content.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Content
+    {
+        [Key]
+        public int ContentID { get; set; }
+
+        [StringLength(1000)]
+        public string ContentValue { get; set; }
+        public DateTime ContentDate { get; set; }
+
+        // ContentYazar
+        // ContentBaşlık
+        // İlişkili alanlardan gelen değerlere kısıtlama getirmiyoruz
+        public int HeadingID { get; set; }
+        public virtual Heading Heading { get; set; }
+
+        // ? eklersek nullable type olur
+        public int? WriterID { get; set; }
+        public virtual Writer Writer { get; set; }
+    }
+}
